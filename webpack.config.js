a93772ec5c74e2ac99fd67a2e3c6d1d53128245c
LuisLiraC <luis_ssbm@hotmail.com>
2019-06-04 21:57:51 -0500

    Ok...I'm still trying to fix this

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = (env) => {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'js/[name].js',
-      publicPath: path.resolve(__dirname, 'dist')+"/",
+      publicPath: "/",
       chunkFilename: 'js/[id].[chunkhash].js',
     },
     devServer: {
@@ -60,7 +60,7 @@ module.exports = (env) => {
           use: {
             loader: 'url-loader',
             options: {
-              limit: 100000,
+              limit: 1000000,
               fallback: 'file-loader',
               name: 'images/[name].[ext]',
             }
