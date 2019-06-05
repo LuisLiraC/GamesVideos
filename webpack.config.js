70e3ff5310ec690d995a8a51703d90e05fc03889
LuisLiraC <luis_ssbm@hotmail.com>
2019-06-04 21:31:52 -0500

    Here we go again...

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = (env) => {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'js/[name].js',
-      publicPath: "./",
+      publicPath: path.resolve(__dirname, 'dist')+"/",
       chunkFilename: 'js/[id].[chunkhash].js',
     },
     devServer: {
